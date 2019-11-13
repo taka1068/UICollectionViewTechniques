3036e168017d07cdb82fb9d3307749e4493cd3d8
TakanoriHirobe <london.2xxx@gmail.com>
2019-11-13 22:34:50 +0900

    create project

---
+++ UICollectionViewTechniques/UICollectionViewTechniques/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewTechniques
+//
+//  Created by takanori on 2019/11/13.
+//  Copyright © 2019 takanori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
