89deba5d890ae4788e9341b8b2b20ba70917b0de
TakanoriHirobe <london.2xxx@gmail.com>
2019-11-16 23:30:42 +0900

    Navigate to Examples

---
+++ UICollectionViewTechniques/UICollectionViewTechniques/Circular1/Circular1ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Circular1ViewController.swift
+//  UICollectionViewTechniques
+//
+//  Created by takanori on 2019/11/16.
+//  Copyright © 2019 takanori. All rights reserved.
+//
+
+import UIKit
+
+final class Circular1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+    
+}
--- UICollectionViewTechniques/UICollectionViewTechniques/RootViewController.swift
+++ UICollectionViewTechniques/UICollectionViewTechniques/RootViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
+enum Contents: CaseIterable {
+    case circular1
+}
+
+extension Contents {
+    var viewController: UIViewController.Type {
+        switch self {
+        case .circular1:
+            return Circular1ViewController.self
+        }
+    }
+    
+    var title: String {
+        return String(describing: self.viewController)
+    }
+}
+
+private let cellIdentifier = "cell"
+
 class RootViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.flashScrollIndicators()
+    }
 }
 
+extension RootViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Contents.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = Contents.allCases[indexPath.row].title
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    
+}
+
+extension RootViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let content = Contents.allCases[indexPath.row]
+        let viewController = content.viewController.init()
+        viewController.title = content.title
+        navigationController?.pushViewController(viewController, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
